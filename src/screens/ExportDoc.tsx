6176ee2ec81007a715dff4799ccad00c48c9aec4
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-04-21 10:51:04 +0200

    draft pdf content

--- src/screens/ExportDoc.tsx
+++ src/screens/ExportDoc.tsx
@@ -87,7 +87,9 @@ const ExportDoc: React.FC<Props> = ({
         
         <button 
           onClick={()=>{
-            const pdfPage1 = document.getElementById("pdf_page_1");
+            const pdfPage1Element = document.getElementById("pdf_page_1");
+            // @ts-ignore
+            var pdfPage1 = pdfPage1Element.cloneNode(true);
             console.log('pdfPage1', pdfPage1);
             const newWindow = window.open();
             // @ts-ignore
@@ -118,65 +120,264 @@ const ExportDoc: React.FC<Props> = ({
         {/* ____________________________________________________________________  */}
 
         <div 
-          id="pdf_page_1"
           style={{
-            width: "1900px",
-            height: "1000px",
             position: "fixed",
             top: 0,
             left: 293,
-            backgroundColor: "white",
             zIndex: 99999,
           }}
         >
-          <h1>Hittestress PET rapport</h1>
-          <h2> Kaarten</h2>
-          <table>
-            <tr>
-              <td>
-                <h3>Huidige hittestress</h3>
-                <p>Uitleg over de huidige hittestress</p>
-              </td>
-              <td>
-                <Map
-                  style={{
-                    width: "300px",
-                    height: "500px",
-                  }}
-                  zoomControl={false}
-                  bounds={initialBounds}
-                  doubleClickZoom={false}
-                  closePopupOnClick={false}
-                  dragging={false}
-                  // @ts-ignore \*}
-                  // zoomSnap={false}
-                  // @ts-ignore 
-                  // zoomDelta={false}
-                  trackResize={false}
-                  touchZoom={false}
-                  scrollWheelZoom={false}
-                >
-                  <TileLayer
-                    // @ts-ignore
-                    url={`https://api.mapbox.com/styles/v1/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6/tiles/256/{z}/{x}/{y}@2x?access_token=${configuration.mapboxAccessToken}`}
-                    zIndex={0}
-                  />
-                  <WMSTileLayer
-                    key="heatstress-original"
-                    url="http://nxt3.staging.lizard.net/wms/"
-                    // @ts-ignore
-                    layers={configuration.originalHeatstressLayer}
-                    // @ts-ignore
-                    styles={configuration.heatstressStyle}
-                    updateWhenIdle={true}
-                    updateWhenZooming={false}
-                    updateInterval={1000}
-                  />
-                </Map>
+          <div
+            id="pdf_page_1"
+            style={{
+              width: "1000px",
+              height: "19000px",
+              backgroundColor: "white",
+              color: "black",
+            }}
+          >
+            <h1>Hittestress PET rapport</h1>
+            <h2> Kaarten</h2>
+            <table>
+              <tbody>
+              <tr>
+                <td>
+                  <h3>Huidige hittestress</h3>
+                  <p>Uitleg over de huidige hittestress</p>
+                </td>
+                <td>
+                  <Map
+                    style={{
+                      width: "400px",
+                      height: "300px",
+                    }}
+                    zoomControl={false}
+                    bounds={initialBounds}
+                    doubleClickZoom={false}
+                    closePopupOnClick={false}
+                    dragging={false}
+                    // @ts-ignore \*}
+                    // zoomSnap={false}
+                    // @ts-ignore 
+                    // zoomDelta={false}
+                    trackResize={false}
+                    touchZoom={false}
+                    scrollWheelZoom={false}
+                  >
+                    <TileLayer
+                      // @ts-ignore
+                      url={`https://api.mapbox.com/styles/v1/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6/tiles/256/{z}/{x}/{y}@2x?access_token=${configuration.mapboxAccessToken}`}
+                      zIndex={0}
+                    />
+                    <WMSTileLayer
+                      key="heatstress-original"
+                      url="http://nxt3.staging.lizard.net/wms/"
+                      // @ts-ignore
+                      layers={configuration.originalHeatstressLayer}
+                      // @ts-ignore
+                      styles={configuration.heatstressStyle}
+                      updateWhenIdle={true}
+                      updateWhenZooming={false}
+                      updateInterval={1000}
+                    />
+                  </Map>
 
-              </td>
-            </tr>
-          </table>
+                </td>
+              </tr>
+              <tr>
+              <td>
+                  <h3>Hittestress na maatregelen</h3>
+                  <p>Uitleg over hittestress na maatregelen</p>
+                </td>
+                <td>
+                  <Map
+                    style={{
+                      width: "400px",
+                      height: "300px",
+                    }}
+                    zoomControl={false}
+                    bounds={initialBounds}
+                    doubleClickZoom={false}
+                    closePopupOnClick={false}
+                    dragging={false}
+                    // @ts-ignore \*}
+                    // zoomSnap={false}
+                    // @ts-ignore 
+                    // zoomDelta={false}
+                    trackResize={false}
+                    touchZoom={false}
+                    scrollWheelZoom={false}
+                  >
+                    <TileLayer
+                      // @ts-ignore
+                      url={`https://api.mapbox.com/styles/v1/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6/tiles/256/{z}/{x}/{y}@2x?access_token=${configuration.mapboxAccessToken}`}
+                      zIndex={0}
+                    />
+                    <WMSTileLayer
+                      key="heatstress-original"
+                      url="http://nxt3.staging.lizard.net/wms/"
+                      // @ts-ignore
+                      layers={configuration.originalHeatstressLayer}
+                      // @ts-ignore
+                      styles={configuration.heatstressStyle}
+                      updateWhenIdle={true}
+                      updateWhenZooming={false}
+                      updateInterval={1000}
+                    />
+                  </Map>
+
+                </td>
+              </tr>
+              <tr>
+              <td>
+                  <h3>Verschil in hittestress</h3>
+                  <p>Uitleg over verschil in hittestress</p>
+                </td>
+                <td>
+                  <Map
+                    style={{
+                      width: "400px",
+                      height: "300px",
+                    }}
+                    zoomControl={false}
+                    bounds={initialBounds}
+                    doubleClickZoom={false}
+                    closePopupOnClick={false}
+                    dragging={false}
+                    // @ts-ignore \*}
+                    // zoomSnap={false}
+                    // @ts-ignore 
+                    // zoomDelta={false}
+                    trackResize={false}
+                    touchZoom={false}
+                    scrollWheelZoom={false}
+                  >
+                    <TileLayer
+                      // @ts-ignore
+                      url={`https://api.mapbox.com/styles/v1/nelenschuurmans/ck8sgpk8h25ql1io2ccnueuj6/tiles/256/{z}/{x}/{y}@2x?access_token=${configuration.mapboxAccessToken}`}
+                      zIndex={0}
+                    />
+                    <WMSTileLayer
+                      key="heatstress-original"
+                      url="http://nxt3.staging.lizard.net/wms/"
+                      // @ts-ignore
+                      layers={configuration.originalHeatstressLayer}
+                      // @ts-ignore
+                      styles={configuration.heatstressStyle}
+                      updateWhenIdle={true}
+                      updateWhenZooming={false}
+                      updateInterval={1000}
+                    />
+                  </Map>
+
+                </td>
+              </tr>
+              </tbody>
+            </table>
+            <h1>Hittestress PET rapport</h1>
+            <h2> Verdeling gevoelstemperatuur</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <div
+                      style={{
+                        height: "300px",
+                        width: "400px"
+                      }}
+                    >
+                    <h1>Todo Bar chart temperature</h1>
+                    </div>
+                  </td>
+                  <td>
+                      <h3>Gemiddelde gevoelstemperatuur</h3>
+                      <p>De gemiddelde temperatuur in dit gebied, voor en na de maatregelen. </p>
+                      <b>Belangrijk:</b>
+                      <br/>
+                      <p>
+                      Er kunnen grote verschillen zitten de verharding van een gebied (denk aan een grote strook groen, maar ook een grote parkeerplaats). Daarom kunnen deze cijfers een vertekend beeld geven.
+                      </p>
+                      <table>
+                        <thead>
+                          <tr>
+                            <td><b>Huidige situatie</b></td>
+                            <td><b>Huidige situatie</b></td>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>
+                              31°
+                            </td>
+                            <td>
+                              26.5°
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <h2> Statistieken</h2>
+            <table>
+              <thead>
+                <tr>
+                  <td>
+                      Indicatoren
+                  </td>
+                  <td>
+                      Huidige situatie
+                  </td>
+                  <td>
+                      Na maatregelen
+                  </td>
+                </tr>
+              </thead>
+              <tbody>
+                    <tr>
+                      <td>
+                        Percentage schaduw
+                      </td>
+                      <td>
+                        20%
+                      </td>
+                      <td>
+                        40%
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Percentage bomen
+                      </td>
+                      <td>
+                        10%
+                      </td>
+                      <td>
+                        15%
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        Groen vs. verhard oppervlak
+                      </td>
+                      <td>
+                        30/70
+                      </td>
+                      <td>
+                        30/70
+                      </td>
+                    </tr>
+              </tbody>
+            </table>
+            <h2> Over de hittestress tool</h2>
+            <p>
+              <b>PET staat voor 'Physiological Equivalent Temperature'. </b>De kaart laat de gevoelstemperatuur in de buitenruimte zien. Op het Kennisportaal Ruimtelijke Adaptatie vindt u een duidelijke uitleg over hoe een PET-kaart wordt opgebouwd volgens de landelijke methodiek. 
+            </p>
+            <p>
+            Voor het berekenen van de PET-hittestresskaart, worden veel verschillende informatiebronnen gebruikt. Denk daarbij aan KNMI meteogegevens (luchttemperatuur, luchtvochtigheid, en straling), en gegevens over het gebied zelf (zoals schaduw, groenfactor en skyviewfactor). Met de Lizard Geoblocks module worden al deze kaarten on-the-fly gecombineerd tot de PET-hittestresskaart. Het voordeel is dat wanneer een van de bronnen geactualiseerd wordt, de PET-hittestresskaart automatisch herberekend wordt met de meest actuele gegevens. De oude kaarten zijn natuurlijk ook nog beschikbaar!
+            </p>
+          </div>
         </div>
       
       
