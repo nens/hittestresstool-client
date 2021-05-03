448c3502c8e2a3af97995af87d23547633903496
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-05-03 15:48:18 +0200

    export geojson

--- src/screens/ExportGeoJson.tsx
+++ src/screens/ExportGeoJson.tsx
@@ -2,9 +2,6 @@ import React  from 'react';
 import { connect, useSelector,  } from 'react-redux';
 import Block from '../components/Block';
 import DownloadIcon from '../icons/Download';
-import {
-  addMessage,
-} from '../state/message';
  import {
   getReportPolygonsOnMap,
  } from '../state/reportPolygons';
@@ -16,13 +13,25 @@ import {
 } from '../state/pavements';
 
 
-interface Props {
-  addMessage: (message: string) => void,
+interface Props {}
+
+const download = (filename: string, text: string) => {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
 
-const ExportDoc: React.FC<Props> = ({
-  addMessage,
-}) => {
+// Start file download.
+
+
+const ExportDoc: React.FC<Props> = () => {
 
   const treesOnMap = useSelector(getTreesOnMap);
   const pavementsOnMap = useSelector(getPavementsOnMap);
@@ -30,7 +39,11 @@ const ExportDoc: React.FC<Props> = ({
 
   console.log( "geojson", treesOnMap, pavementsOnMap, reportPolygonsOnMap );
   
-  // const completeGeoJson = 
+  const completeGeoJson = {
+    type: "FeatureCollection",
+    // @ts-ignore 
+    features: [].concat(treesOnMap.features).concat(pavementsOnMap.features).concat(reportPolygonsOnMap)
+  }
 
  
 
@@ -43,6 +56,7 @@ const ExportDoc: React.FC<Props> = ({
       status={"closed"} 
       onClick={()=>{
         console.log('clicked');
+        download("Hittestress_GeoJSON.txt", JSON.stringify(completeGeoJson, null, "    "));
       }}
     >
 
@@ -52,5 +66,4 @@ const ExportDoc: React.FC<Props> = ({
 };
 
 export default connect(null, {
-  addMessage,  
 })(ExportDoc);
