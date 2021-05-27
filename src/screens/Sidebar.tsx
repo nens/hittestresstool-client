0e21ccb478fe6c1a56f38a626bd1b45b3e5ba097
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2021-05-27 12:12:43 +0200

    info button draft

--- src/screens/Sidebar.tsx
+++ src/screens/Sidebar.tsx
@@ -8,12 +8,36 @@ import ExportDoc from './ExportDoc';
 import ExportGeoJson from './ExportGeoJson';
 import ImportGeoJson from './ImportGeoJson';
 import packageJson from '../../package.json';
+import center from '@turf/center';
 
 const Sidebar: React.FC = () => {
 
   return (
     <div>
-      <h1 title={"client-version: " +packageJson.version}>Hittestresstool</h1>
+      <h1 
+        title={"client-version: " +packageJson.version}
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <span>Hittestresstool</span>
+        <button
+          style={{
+            backgroundColor: "white",
+            color: "#6DC1A9",
+            fontSize: "29px",
+            borderRadius: "18px",
+            width: "36px",
+            height: "36px",
+            border: "none",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >i</button>
+      </h1>
       <h2>Resultaat</h2>
       <BlockHittestress/>
       <h2>Brondata</h2>
